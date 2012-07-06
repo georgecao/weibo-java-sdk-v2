ab8d4c88ec210b110dcf90b2b142b1d809cf3946
George Cao <matrix3456@163.com>
2012-07-06 10:46:32 +0800

    refactoring

--- src/main/java/weibo4j/util/ParamUtils.java
+++ src/main/java/weibo4j/util/ParamUtils.java
@@ -4,13 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import weibo4j.model.AccountUser;
 import weibo4j.model.PostParameter;
+import weibo4j.model.Required;
 import weibo4j.model.SerializedName;
 
-import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -35,6 +34,7 @@ public class ParamUtils {
         for (Field field : fields) {
             map.put(field.getName(), field);
         }
+        List<String> missed = new LinkedList<String>();
         try {
             PropertyDescriptor[] pds = Introspector.getBeanInfo(AccountUser.class).getPropertyDescriptors();
             for (PropertyDescriptor pd : pds) {
@@ -45,18 +45,26 @@ public class ParamUtils {
                     if (field.isAnnotationPresent(SerializedName.class)) {
                         paramName = field.getAnnotation(SerializedName.class).value();
                     }
+                    boolean isRequired = field.isAnnotationPresent(Required.class);
                     Object o = pd.getReadMethod().invoke(user);
+                    String paramValue = "";
                     if (o != null) {
-                        params.add(new PostParameter(paramName, o.toString()));
+                        paramValue = o.toString();
                     }
+                    if (null == paramValue || paramValue.trim().length() == 0) {
+                        if (isRequired) {
+                            missed.add(paramName);
+                        }
+                        continue;
+                    }
+                    params.add(new PostParameter(paramName, paramValue));
                 }
             }
-        } catch (IntrospectionException e) {
+        } catch (Exception e) {
             LOG.error("Error occurred:", e);
-        } catch (InvocationTargetException e) {
-            LOG.error("Error occurred:", e);
-        } catch (IllegalAccessException e) {
-            LOG.error("Error occurred:", e);
+        }
+        if (missed.size() > 0) {
+            throw new IllegalArgumentException("These parameters (" + missed + ") are required, but not specified.");
         }
         return params;
     }
