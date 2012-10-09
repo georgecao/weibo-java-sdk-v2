9615d170315a24ad313b3c0ff7b94a14539c212c
George Cao <matrix3456@163.com>
2012-10-09 17:33:44 +0800

    remove xml parser, only support json

--- src/main/java/weibo4j/model/WeiboResponse.java
+++ src/main/java/weibo4j/model/WeiboResponse.java
@@ -26,13 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package weibo4j.model;
 
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import weibo4j.http.HTMLEntity;
 import weibo4j.http.Response;
 
 import java.io.Serializable;
@@ -81,107 +75,19 @@ public class WeiboResponse implements Serializable {
         }
     }
 
-    protected static void ensureRootNodeNameIs(String rootName, Element elem) throws WeiboException {
-        if (!rootName.equals(elem.getNodeName())) {
-            throw new WeiboException("Unexpected root node name:" + elem.getNodeName() + ". Expected:" + rootName + ". Check the availability of the Weibo API at http://open.t.sina.com.cn/.");
-        }
-    }
-
-    protected static void ensureRootNodeNameIs(String[] rootNames, Element elem) throws WeiboException {
-        String actualRootName = elem.getNodeName();
-        for (String rootName : rootNames) {
-            if (rootName.equals(actualRootName)) {
-                return;
-            }
-        }
-        String expected = "";
-        for (int i = 0; i < rootNames.length; i++) {
-            if (i != 0) {
-                expected += " or ";
-            }
-            expected += rootNames[i];
-        }
-        throw new WeiboException("Unexpected root node name:" + elem.getNodeName() + ". Expected:" + expected + ". Check the availability of the Weibo API at http://open.t.sina.com.cn/.");
-    }
-
-    protected static void ensureRootNodeNameIs(String rootName, Document doc) throws WeiboException {
-        Element elem = doc.getDocumentElement();
-        if (!rootName.equals(elem.getNodeName())) {
-            throw new WeiboException("Unexpected root node name:" + elem.getNodeName() + ". Expected:" + rootName + ". Check the availability of the Weibo API at http://open.t.sina.com.cn/");
-        }
-    }
-
-    protected static boolean isRootNodeNilClasses(Document doc) {
-        String root = doc.getDocumentElement().getNodeName();
-        return "nil-classes".equals(root) || "nilclasses".equals(root);
-    }
-
-    protected static String getChildText(String str, Element elem) {
-        return HTMLEntity.unescape(getTextContent(str, elem));
-    }
-
-    protected static String getTextContent(String str, Element elem) {
-        NodeList nodelist = elem.getElementsByTagName(str);
-        if (nodelist.getLength() > 0) {
-            Node node = nodelist.item(0).getFirstChild();
-            if (null != node) {
-                String nodeValue = node.getNodeValue();
-                return null != nodeValue ? nodeValue : "";
-            }
-        }
-        return "";
-    }
-
-    /*modify by sycheng  add "".equals(str) */
-    protected static int getChildInt(String str, Element elem) {
-        String str2 = getTextContent(str, elem);
-        if (null == str2 || "".equals(str2) || "null".equals(str)) {
-            return -1;
-        } else {
-            return Integer.valueOf(str2);
-        }
-    }
-
-    protected static long getChildLong(String str, Element elem) {
-        String str2 = getTextContent(str, elem);
-        if (null == str2 || "".equals(str2) || "null".equals(str)) {
-            return -1;
-        } else {
-            return Long.valueOf(str2);
-        }
-    }
-
     protected static String getString(String name, JSONObject json) {
         return getString(name, json, false);
     }
 
     protected static String getString(String name, JSONObject json, boolean decode) {
-        String returnValue = null;
-        try {
-            returnValue = json.getString(name);
-            if (decode) {
-                try {
-                    returnValue = URLDecoder.decode(returnValue, "UTF-8");
-                } catch (UnsupportedEncodingException ignore) {
-                }
+        String returnValue = json.optString(name);
+        if (decode) {
+            try {
+                returnValue = URLDecoder.decode(returnValue, "UTF-8");
+            } catch (UnsupportedEncodingException ignore) {
             }
-        } catch (JSONException ignore) {
-            // refresh_url could be missing
         }
         return returnValue;
-    }
-
-    protected static boolean getChildBoolean(String str, Element elem) {
-        String value = getTextContent(str, elem);
-        return Boolean.valueOf(value);
-    }
-
-    protected static Date getChildDate(String str, Element elem) throws WeiboException {
-        return getChildDate(str, elem, DATE_FORMAT);
-    }
-
-    protected static Date getChildDate(String str, Element elem, String format) throws WeiboException {
-        return parseDate(getChildText(str, elem), format);
     }
 
     protected static Date parseDate(String str, String format) throws WeiboException {
@@ -198,28 +104,52 @@ public class WeiboResponse implements Serializable {
         }
     }
 
-    protected static int getInt(String key, JSONObject json) {
-        String str = json.optString(key);
-        if (isEmpty(str) || NULL_STRING.equals(str)) {
-            return -1;
+    private static interface Converter<T, V> {
+        V cast(T from);
+    }
+
+    private static Converter<String, Long> string2Long = new Converter<String, Long>() {
+        @Override
+        public Long cast(String from) {
+            return Long.valueOf(from);
         }
-        return Integer.valueOf(str);
+    };
+
+    private static Converter<String, Boolean> string2Bool = new Converter<String, Boolean>() {
+        @Override
+        public Boolean cast(String from) {
+            return Boolean.valueOf(from);
+        }
+    };
+    private static Converter<String, Integer> string2Int = new Converter<String, Integer>() {
+        @Override
+        public Integer cast(String from) {
+            return Integer.valueOf(from);
+        }
+    };
+
+    private static <V> V getValue(String key, JSONObject json, Converter<String, V> converter, V defaultValue) {
+        String value = json.optString(key);
+        if (validate(value)) {
+            return converter.cast(value);
+        }
+        return defaultValue;
+    }
+
+    protected static int getInt(String key, JSONObject json) {
+        return getValue(key, json, string2Int, -1);
+    }
+
+    private static boolean validate(String value) {
+        return !(isEmpty(value) || NULL_STRING.equals(value));
     }
 
     protected static long getLong(String key, JSONObject json) {
-        String str = json.optString(key);
-        if (isEmpty(str) || NULL_STRING.equals(str)) {
-            return -1;
-        }
-        return Long.valueOf(str);
+        return getValue(key, json, string2Long, -1L);
     }
 
     protected static boolean getBoolean(String key, JSONObject json) {
-        String str = json.optString(key);
-        if (isEmpty(str) || NULL_STRING.equals(str)) {
-            return false;
-        }
-        return Boolean.valueOf(str);
+        return getValue(key, json, string2Bool, false);
     }
 
     public int getRateLimitLimit() {
